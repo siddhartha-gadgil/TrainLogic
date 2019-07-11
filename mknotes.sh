bd43faf4b1ce360f0ead98da59ae9ebada023436
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2019-07-11 13:48:58 +0530

    notebooks in navbar, with date

--- mknotes.sh
+++ mknotes.sh
@@ -4,6 +4,8 @@ for note in notes/*.ipynb
 do
     echo $note   
     jupyter nbconvert --to html --output $note.html $note
-    echo -e "---\n---\n" > docs/_notebooks/$note.html
+    MODDATE=$(stat -c %y build.sc)
+    echo $MODDATE
+    echo -e "---\ndate: $MODDATE\n---\n" > docs/_notebooks/$note.html
     cat notes/$note.html >> docs/_notebooks/$note.html
 done
